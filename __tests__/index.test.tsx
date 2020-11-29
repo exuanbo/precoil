d005de9b2a0ead283409925cc87fd027bbd0386c
exuanbo <exuanbo@protonmail.com>
2020-11-29 00:48:01 +0000

    test: minor

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -3,12 +3,11 @@ import { render } from 'react-dom'
 import { PrecoilRoot } from '../src/index'
 
 it('should work', () => {
-  const div = document.createElement('div')
   const App = (): JSX.Element => <h1>Precoil</h1>
   render(
     <PrecoilRoot>
       <App />
     </PrecoilRoot>,
-    div
+    document.createElement('body')
   )
 })
