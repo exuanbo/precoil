ceb38204f325e703d66fbcb05f66f5ccba74e992
exuanbo <exuanbo@protonmail.com>
2021-02-15 10:00:01 +0000

    test: add task

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -4,6 +4,7 @@ import '@testing-library/jest-dom'
 import { atom, useAtom } from '../src/index'
 
 const input = atom<string>()
+const numberInput = atom(0)
 const inputWithDefault = atom('I am a default value')
 
 const Input: FunctionComponent = () => {
@@ -27,6 +28,28 @@ const UpperCaseInput: FunctionComponent = () => {
   return <span data-testid="uppercase-input">{value?.toUpperCase() ?? ''}</span>
 }
 
+const ComputedInput: FunctionComponent = () => {
+  const [value, setValue] = useAtom(numberInput)
+  const getNumber = (value: number | string): number => {
+    const parsedNumber = Number(value)
+    if (isNaN(parsedNumber)) {
+      return 0
+    }
+    return parsedNumber
+  }
+  return (
+    <>
+      <input
+        aria-label="number-input"
+        onChange={e =>
+          setValue(prevValue => prevValue + getNumber(e.target.value))
+        }
+      />
+      <span data-testid="computed-input">{value}</span>
+    </>
+  )
+}
+
 const InputWithDefault: FunctionComponent = () => {
   const [value, setValue] = useAtom(inputWithDefault)
   return (
@@ -43,6 +66,7 @@ const App: FunctionComponent = () => (
     <Input />
     <MirrorInput />
     <UpperCaseInput />
+    <ComputedInput />
     <InputWithDefault />
   </>
 )
@@ -51,6 +75,8 @@ interface Setup {
   input: HTMLInputElement
   mirrorInput: HTMLElement
   upperCaseInput: HTMLElement
+  numberInput: HTMLInputElement
+  computedInput: HTMLElement
   inputWithDefault: HTMLInputElement
 }
 
@@ -59,6 +85,8 @@ const setup = (): Setup => {
   const input = screen.getByLabelText('input') as HTMLInputElement
   const mirrorInput = screen.getByTestId('mirror-input')
   const upperCaseInput = screen.getByTestId('uppercase-input')
+  const numberInput = screen.getByLabelText('number-input') as HTMLInputElement
+  const computedInput = screen.getByTestId('computed-input')
   const inputWithDefault = screen.getByLabelText(
     'input-with-default'
   ) as HTMLInputElement
@@ -66,6 +94,8 @@ const setup = (): Setup => {
     input,
     mirrorInput,
     upperCaseInput,
+    numberInput,
+    computedInput,
     inputWithDefault
   }
 }
@@ -77,6 +107,15 @@ it('should work', () => {
   expect(input.value).toBe('hi')
   expect(mirrorInput).toHaveTextContent('hi')
   expect(upperCaseInput).toHaveTextContent('HI')
+})
+
+it('should work when setState accepts a function', () => {
+  const { numberInput, computedInput } = setup()
+  expect(computedInput).toHaveTextContent('0')
+  fireEvent.change(numberInput, { target: { value: '1' } })
+  expect(computedInput).toHaveTextContent('1')
+  fireEvent.change(numberInput, { target: { value: '2' } })
+  expect(computedInput).toHaveTextContent('3')
 })
 
 it('should work with default value', () => {
