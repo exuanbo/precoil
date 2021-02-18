318adf838381a3a741893a08052e942a83b8e172
exuanbo <exuanbo@protonmail.com>
2021-02-18 22:15:45 +0000

    refactor: class `Atom`

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,14 +1,20 @@
 import React, { FunctionComponent } from 'react'
 import { render as rtlRender, fireEvent, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import { atom, useAtom, useAtomReducer } from '../src/index'
+import { atom } from '../src'
 
 const textState = atom<string>()
-const numberStore = atom({ number: 0 })
+
+const countStore = atom({ count: 0 })
+
+const unsubscribe = countStore.subscribe(state => {
+  console.log(`State has been changed to ${state.count}.`)
+})
+
 const textStateWithDefault = atom('I am a default value')
 
 const Input: FunctionComponent = () => {
-  const [text, setText] = useAtom(textState)
+  const [text, setText] = textState.useState()
   return (
     <input
       aria-label="input"
@@ -19,25 +25,25 @@ const Input: FunctionComponent = () => {
 }
 
 const MirrorInput: FunctionComponent = () => {
-  const [text] = useAtom(textState)
+  const [text] = textState.useState()
   return <span data-testid="mirror-input">{text ?? ''}</span>
 }
 
 const UpperCaseInput: FunctionComponent = () => {
-  const [text] = useAtom(textState)
+  const [text] = textState.useState()
   return <span data-testid="uppercase-input">{text?.toUpperCase() ?? ''}</span>
 }
 
-const ComputedInput: FunctionComponent = () => {
-  const [{ number }, setNumberStore] = useAtom(numberStore)
-  const dispatch = useAtomReducer((state, action) => {
+const Counter: FunctionComponent = () => {
+  const [state, setState] = countStore.useState()
+  const dispatch = countStore.useReducer((state, action) => {
     switch (action.type) {
-      case 'increment':
-        return { number: state.number + 1 }
+      case 'INCREMENT':
+        return { count: state.count + 1 }
       default:
         return state
     }
-  }, numberStore)[1]
+  })[1]
   const getNumber = (value: number | string): number => {
     const parsedNumber = Number(value)
     if (isNaN(parsedNumber)) {
@@ -50,16 +56,16 @@ const ComputedInput: FunctionComponent = () => {
       <input
         aria-label="number-input"
         onChange={e =>
-          setNumberStore(({ number }) => ({
-            number: number + getNumber(e.target.value)
+          setState(({ count }) => ({
+            count: count + getNumber(e.target.value)
           }))
         }
       />
-      <span data-testid="computed-input">{number}</span>
-      <button data-testid="inc" onClick={() => dispatch({ type: 'increment' })}>
+      <span data-testid="counter">{state.count}</span>
+      <button data-testid="inc" onClick={() => dispatch({ type: 'INCREMENT' })}>
         inc
       </button>
-      <button data-testid="reset" onClick={() => setNumberStore({ number: 0 })}>
+      <button data-testid="reset" onClick={() => setState({ count: 0 })}>
         reset
       </button>
     </>
@@ -67,7 +73,7 @@ const ComputedInput: FunctionComponent = () => {
 }
 
 const InputWithDefault: FunctionComponent = () => {
-  const [text, setText] = useAtom(textStateWithDefault)
+  const [text, setText] = textStateWithDefault.useState()
   return (
     <input
       aria-label="input-with-default"
@@ -82,7 +88,7 @@ const App: FunctionComponent = () => (
     <Input />
     <MirrorInput />
     <UpperCaseInput />
-    <ComputedInput />
+    <Counter />
     <InputWithDefault />
   </>
 )
@@ -92,7 +98,7 @@ interface Setup {
   mirrorInput: HTMLElement
   upperCaseInput: HTMLElement
   numberInput: HTMLInputElement
-  computedInput: HTMLElement
+  counter: HTMLElement
   incBtn: HTMLElement
   resetBtn: HTMLElement
   inputWithDefault: HTMLInputElement
@@ -104,7 +110,7 @@ const setup = (): Setup => {
   const mirrorInput = screen.getByTestId('mirror-input')
   const upperCaseInput = screen.getByTestId('uppercase-input')
   const numberInput = screen.getByLabelText('number-input') as HTMLInputElement
-  const computedInput = screen.getByTestId('computed-input')
+  const counter = screen.getByTestId('counter')
   const incBtn = screen.getByTestId('inc')
   const resetBtn = screen.getByTestId('reset')
   const inputWithDefault = screen.getByLabelText(
@@ -115,7 +121,7 @@ const setup = (): Setup => {
     mirrorInput,
     upperCaseInput,
     numberInput,
-    computedInput,
+    counter,
     incBtn,
     resetBtn,
     inputWithDefault
@@ -125,39 +131,56 @@ const setup = (): Setup => {
 it('should work', () => {
   const { input, mirrorInput, upperCaseInput } = setup()
   expect(input.value).toBe('')
+
   fireEvent.change(input, { target: { value: 'hi' } })
   expect(input.value).toBe('hi')
+
   expect(mirrorInput).toHaveTextContent('hi')
   expect(upperCaseInput).toHaveTextContent('HI')
 })
 
 it('should work when setState accepts a function', () => {
-  const { numberInput, computedInput } = setup()
-  expect(computedInput).toHaveTextContent('0')
+  const { numberInput, counter } = setup()
+  expect(counter).toHaveTextContent('0')
+
   fireEvent.change(numberInput, { target: { value: '1' } })
-  expect(computedInput).toHaveTextContent('1')
+  expect(counter).toHaveTextContent('1')
+
   fireEvent.change(numberInput, { target: { value: '2' } })
-  expect(computedInput).toHaveTextContent('3')
+  expect(counter).toHaveTextContent('3')
 })
 
-it('should work when setState accepts a function', () => {
-  const { numberInput, computedInput, incBtn, resetBtn } = setup()
-  expect(computedInput).toHaveTextContent('3')
+it('should work with `atom.useReducer`', () => {
+  const { numberInput, counter, incBtn, resetBtn } = setup()
+  expect(counter).toHaveTextContent('3')
+
   fireEvent.change(numberInput, { target: { value: '1' } })
-  expect(computedInput).toHaveTextContent('4')
+  expect(counter).toHaveTextContent('4')
+
   fireEvent.click(incBtn)
-  expect(computedInput).toHaveTextContent('5')
+  expect(counter).toHaveTextContent('5')
+
   fireEvent.change(numberInput, { target: { value: '2' } })
-  expect(computedInput).toHaveTextContent('7')
+  expect(counter).toHaveTextContent('7')
+
   fireEvent.click(incBtn)
-  expect(computedInput).toHaveTextContent('8')
+  expect(counter).toHaveTextContent('8')
+
+  unsubscribe()
+
   fireEvent.click(resetBtn)
-  expect(computedInput).toHaveTextContent('0')
+  expect(counter).toHaveTextContent('0')
+
+  countStore.distroy()
+
+  fireEvent.click(incBtn)
+  expect(counter).toHaveTextContent('0')
 })
 
 it('should work with default value', () => {
   const { inputWithDefault } = setup()
   expect(inputWithDefault.value).toBe('I am a default value')
+
   fireEvent.change(inputWithDefault, { target: { value: 'Now I am changed' } })
   expect(inputWithDefault.value).toBe('Now I am changed')
 })
--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,16 @@
 import React, { useState, useEffect, useReducer } from 'react'
 
+type Callback<T> = ((state: T) => void) | (() => void)
 type DispatchSetStateAction<T> = React.Dispatch<React.SetStateAction<T>>
 type DispatchReducerAction<T> = React.Dispatch<
   React.ReducerAction<React.Reducer<T, any>>
 >
 
-type SetStateActionSubscription<T> = Set<DispatchSetStateAction<T>>
-type ReducerActionSubscription<T> = Set<DispatchReducerAction<T>>
-type Subscription<T> =
-  | SetStateActionSubscription<T>
-  | ReducerActionSubscription<T>
+type CallbackSubscription<T> = Set<Callback<T>>
+type DispatchSetStateActionSubscription<T> = Set<DispatchSetStateAction<T>>
+type DispatchReducerActionSubscription<T> = Set<DispatchReducerAction<T>>
 
-interface Atom<T> {
-  value: T
-  setStateSubscription: SetStateActionSubscription<T>
-  reducerSubscription: ReducerActionSubscription<T>
-}
-
-export function atom<T>(initialValue: T): Atom<T>
-export function atom<T>(initialValue?: T): Atom<T | undefined>
-
-export function atom<T>(initialValue: T): Atom<T> {
-  return {
-    value: initialValue,
-    setStateSubscription: new Set(),
-    reducerSubscription: new Set()
-  }
-}
-
-const subscribe = <T>(
-  dispatch: DispatchSetStateAction<T> | DispatchReducerAction<T>,
-  subscription: Subscription<T>
-): (() => void) => {
-  subscription.add(dispatch)
-
-  return () => {
-    subscription.delete(dispatch)
-  }
-}
-
-export function useAtom<T>(atom: Atom<T>): [T, DispatchSetStateAction<T>]
-export function useAtom<T>(
-  atom: Atom<T | undefined>
-): [T | undefined, DispatchSetStateAction<T>]
-
-export function useAtom<T>(atom: Atom<T>): [T, DispatchSetStateAction<T>] {
-  const [state, setState] = useState<T>(atom.value)
-
-  useEffect(() => subscribe(setState, atom.setStateSubscription), [])
-
-  const publishState: DispatchSetStateAction<T> = newState => {
-    atom.setStateSubscription.forEach(setState => {
-      setState(newState)
-    })
-
-    const newStateValue =
-      newState instanceof Function ? newState(atom.value) : newState
-
-    atom.reducerSubscription.forEach(dispatch => {
-      dispatch({ type: '__UPDATE__', newState: newStateValue })
-    })
-
-    atom.value = newStateValue
-  }
-
-  return [state, publishState]
-}
+type Unsubscribe = () => void
 
 interface ReducerAction {
   type: string
@@ -76,41 +21,122 @@ interface ReducerActionWrapper<T> extends ReducerAction {
   newState?: T
 }
 
-export function useAtomReducer<T>(
-  reducer: React.Reducer<T, ReducerAction>,
-  atom: Atom<T>
-): [T, DispatchReducerAction<T>]
-export function useAtomReducer<T>(
-  reducer: React.Reducer<T, ReducerAction>,
-  atom: Atom<T | undefined>
-): [T | undefined, DispatchReducerAction<T>]
+type UseAtomState<T> = () => [T, DispatchSetStateAction<T>]
+type UseAtomReducer<T> = (
+  reducer: React.Reducer<T, ReducerAction>
+) => [T, DispatchReducerAction<T>]
 
-export function useAtomReducer<T>(
-  reducer: React.Reducer<T, ReducerAction>,
-  atom: Atom<T>
-): [T, DispatchReducerAction<T>] {
-  const reducerWrapper = (prevState: T, action: ReducerActionWrapper<T>): T =>
-    action.type === '__UPDATE__' && action.newState !== undefined
-      ? action.newState
-      : reducer(prevState, action)
+class Atom<T> {
+  #state: T
 
-  const [state, dispatch] = useReducer(reducerWrapper, atom.value)
+  #callbackSubscription: CallbackSubscription<T> = new Set()
+  #setStateSubscription: DispatchSetStateActionSubscription<T> = new Set()
+  #dispatchSubscription: DispatchReducerActionSubscription<T> = new Set()
 
-  useEffect(() => subscribe(dispatch, atom.reducerSubscription), [])
-
-  const dispatchAction: DispatchReducerAction<T> = action => {
-    atom.reducerSubscription.forEach(dispatch => {
-      dispatch(action)
-    })
-
-    const newState = reducer(atom.value, action)
-
-    atom.setStateSubscription.forEach(setState => {
-      setState(newState)
-    })
-
-    atom.value = newState
+  constructor(initialState: T) {
+    this.#state = initialState
   }
 
-  return [state, dispatchAction]
+  subscribe = (cb: Callback<T>): Unsubscribe => {
+    this.#callbackSubscription.add(cb)
+    return () => {
+      this.#callbackSubscription.delete(cb)
+    }
+  }
+
+  #callback = (): void => {
+    this.#callbackSubscription.forEach(fn => {
+      fn(this.#state)
+    })
+  }
+
+  #subscribDispatch = <T>(
+    dispatch: DispatchSetStateAction<T> | DispatchReducerAction<T>,
+    subscription:
+      | DispatchSetStateActionSubscription<T>
+      | DispatchReducerActionSubscription<T>
+  ): Unsubscribe => {
+    subscription.add(dispatch)
+    return () => {
+      subscription.delete(dispatch)
+    }
+  }
+
+  useState: UseAtomState<T> = () =>
+    ((atom): ReturnType<UseAtomState<T>> => {
+      const [state, setState] = useState(atom.#state)
+
+      useEffect(
+        () => atom.#subscribDispatch(setState, atom.#setStateSubscription),
+        []
+      )
+
+      const publishState: DispatchSetStateAction<T> = newState => {
+        atom.#setStateSubscription.forEach(setState => {
+          setState(newState)
+        })
+
+        const newStateValue =
+          newState instanceof Function ? newState(atom.#state) : newState
+
+        atom.#dispatchSubscription.forEach(dispatch => {
+          dispatch({ type: 'ATOM_UPDATE', newState: newStateValue })
+        })
+
+        atom.#state = newStateValue
+
+        atom.#callback()
+      }
+
+      return [state, publishState]
+    })(this)
+
+  useReducer: UseAtomReducer<T> = reducer =>
+    ((atom): ReturnType<UseAtomReducer<T>> => {
+      const reducerWrapper = (
+        prevState: T,
+        action: ReducerActionWrapper<T>
+      ): T =>
+        action.type === 'ATOM_UPDATE' && action.newState !== undefined
+          ? action.newState
+          : reducer(prevState, action)
+
+      const [state, dispatch] = useReducer(reducerWrapper, atom.#state)
+
+      useEffect(
+        () => atom.#subscribDispatch(dispatch, atom.#dispatchSubscription),
+        []
+      )
+
+      const dispatchAction: DispatchReducerAction<T> = action => {
+        atom.#dispatchSubscription.forEach(dispatch => {
+          dispatch(action)
+        })
+
+        const newState = reducer(atom.#state, action)
+
+        atom.#setStateSubscription.forEach(setState => {
+          setState(newState)
+        })
+
+        atom.#state = newState
+
+        atom.#callback()
+      }
+
+      return [state, dispatchAction]
+    })(this)
+
+  distroy = (): void => {
+    this.#callbackSubscription.clear()
+    this.#setStateSubscription.clear()
+    this.#dispatchSubscription.clear()
+  }
+}
+
+export function atom<T>(initialState: T): Atom<T>
+export function atom<T>(initialState?: T): Atom<T | undefined>
+
+export function atom<T>(initialState: T): Atom<T> {
+  return new Atom(initialState)
 }
